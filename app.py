18c5e403d28a0ac560d81219e8d9f6313aa13109
fjcobl@proton.me <fjcobl@proton.me>
2026-02-17 11:19:53 +0100

    .

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#@app.route('/')
+#def hello_world():
+#    return 'Hello, World!'
+
+posts = ['aaaaa','bbbbb','cccccc']
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts), posts=posts)
